4386e6c68ddded516979596738b048d5540dea63
wilhelmi <wilhelmi@96018a83-7788-6bc7-8a80-889cfe11136a>
2012-05-22 17:11:06 +0000

    Added cleanup step

---
+++ src/main/java/ucar/ral/gis/services/pipeline/conversion/ScratchDirectoryCleanupProcessor.java
@@ -0,0 +1,32 @@
+package ucar.ral.gis.services.pipeline.conversion;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+
+import ucar.ral.gis.services.OutputFileNameFactory;
+import ucar.ral.gis.services.messages.ConversionRequestMessage;
+import ucar.ral.gis.services.pipeline.Processor;
+
+public class ScratchDirectoryCleanupProcessor implements Processor {
+	
+	public ScratchDirectoryCleanupProcessor(File scratchDirectory, OutputFileNameFactory outputFileNameFactory) {
+		super();
+	}
+
+	public void process(ConversionRequestMessage conversionRequest) {
+		
+		File workingDirectory = conversionRequest.getConversionOutput().getWorkingDirectory();
+		
+		try {
+			
+			FileUtils.deleteDirectory(workingDirectory);
+		} catch (IOException e) {
+			
+			throw new RuntimeException(e);
+		}
+		
+	}
+
+}
